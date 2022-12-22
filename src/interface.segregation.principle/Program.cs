078e8dbda35d5c2474c2232e6dd45a97f09dee6c
Matheus <matheus.campos@edu.unifil.br>
2022-12-22 22:27:36 +0100

    Interface segregation principle

---
+++ src/interface.segregation.principle/Program.cs
@@ -0,0 +1,100 @@
+﻿/* Avoid one interface with a plenty of functionalities
+ * Prefer to break in a smaller interfaces
+ */
+
+public class Program
+{
+    public class Document { }
+
+    public interface IMachine
+    {
+        void Sprint(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+    
+    /*
+     * If OldScanner doesn't scan or fax?
+     * avoid unnecessary implementations like " This machine doesn't scan files " 
+     */
+    public class OldScanner : IMachine
+    {
+        public void Sprint(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    /*--------------------*/
+    /* God
+     */
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+    /*
+     * ---------------------
+     */
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IMultiFunctionalDevice : IScanner, IPrinter //...
+    {
+        
+    }
+    
+    // Delegate
+    // decorator pattern
+    public class MultiFunctionalDevice : IMultiFunctionalDevice
+    {
+        private  IPrinter _printer;
+        private  IScanner _scanner;
+        
+        public MultiFunctionalDevice(IPrinter printer, IScanner scanner)
+        {
+            _printer = printer ?? throw new ArgumentNullException(nameof(printer));
+            _scanner = scanner ?? throw new ArgumentNullException(nameof(scanner));
+        }
+        
+        public void Print(Document d)
+        {
+            _printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            _scanner.Scan(d);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        
+    }
+}
