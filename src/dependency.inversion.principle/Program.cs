a8e91ba144bd9567cf362174ccf71e21bae9b384
Matheus <matheus.campos@edu.unifil.br>
2022-12-25 21:14:41 +0100

    added missing file

---
+++ src/dependency.inversion.principle/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace dependency.inversion.principle;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var parent = new Person { Name = "Adonis" };
+        var child1 = new Person { Name = "Rock" };
+        var child2 = new Person { Name = "Michael" };
+
+        // var relationships = new Relationships();
+        // relationships.AddParentAndChild(parent, child1);
+        // relationships.AddParentAndChild(parent, child2);
+
+        //new Research(relationships);
+
+        /*BetterSolution*/
+        var relationships = new BetterSolution.Relationships();
+        relationships.AddParentAndChild(parent, child1);
+        relationships.AddParentAndChild(parent, child2); 
+        new BetterSolution.Research(relationships);
+    }
+}
