d0d4829c11bab48fb63d13676618985fb2c00904
Matheus <matheus.campos@edu.unifil.br>
2023-02-09 23:42:34 +0100

    moved files

---
+++ src/template.method/TemplateMethod/Game.cs
@@ -0,0 +1,26 @@
+public partial class Program
+{
+    public abstract class Game
+    {
+        public void Run()
+        {
+            Start();
+            while (!HaveWinner)
+                TakeTurn();
+            Console.WriteLine($"Player {WinningPlayer} wins.");
+        }
+
+        protected abstract void Start();
+        protected abstract bool HaveWinner { get; }
+        protected abstract void TakeTurn();
+        protected abstract int WinningPlayer { get; }
+
+        protected int currentPlayer;
+        protected readonly int numberOfPlayers;
+
+        public Game(int numberOfPlayers)
+        {
+            this.numberOfPlayers = numberOfPlayers;
+        }
+    }
+}
